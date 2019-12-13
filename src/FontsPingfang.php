8d1055cabbc842e4a3d9eacda890854d02633dc3
lei.tian <lei.tian@uhouzz.com>
2019-12-13 11:59:12 +0800

    增加苹方字体

---
+++ src/FontsPingfang.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Fonts;
+
+class FontsPingfang
+{
+
+    /**
+     * @return string
+     */
+    public static function getPath(): string
+    {
+        return __DIR__ . '/../assets/PingFang.ttc';
+    }
+
+}
